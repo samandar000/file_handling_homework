cb9350d0651f3738ab400ca1090ce3abad730237
Samandar000 <samandarnfh.2020@gmail.com>
2022-09-21 09:27:03 +0500

    05

--- file05.py
+++ file05.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    i=0
+    k=0
+    for i in f:
+        if i in ('0123456789'):
+            k+=1
+        answer = k , len(f)-k
+    return list(answer)
+f = open('txt_file/data05.txt').read()
+print(main(f)) 
     
 # Read data from file

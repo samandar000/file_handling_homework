a7bf97b0b62bca4b8706e08420b489c87e9e4033
Samandar000 <samandarnfh.2020@gmail.com>
2022-09-21 23:49:21 +0500

    07

--- file07.py
+++ file07.py
@@ -6,25 +6,25 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    k = 0
+#     k = 0
     
-    for i in f:
-        if i in '0123456789':
-            k+=int(i)
-        return k
-f = open('txt_file/data07.txt')
-f = f.read()
-print(main(f))
-
-
-
-
-#     a=0
 #     for i in f:
 #         if i in '0123456789':
-#             a+=int(i)
-#     return a
-# # Read data from file
+#             k+=int(i)
+#         return k
 # f = open('txt_file/data07.txt')
 # f = f.read()
 # print(main(f))
+
+
+
+
+    a=0
+    for i in f:
+        if i in '0123456789':
+            a+=int(i)
+    return a
+# Read data from file
+f = open('txt_file/data07.txt')
+f = f.read()
+print(main(f))
